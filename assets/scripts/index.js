0d22384bdded4091d791f18897b8e49760153065
Nina Pelts <ninapelts@yahoo.com>
2023-11-13 13:16:25 +0100

    adds tasks and saves to localStorage

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,3 +3,37 @@ const DATE = moment().locale('ru').format('DD MMMM YYYY');
 const TIME = moment().format('HH:mm');
 document.getElementById('header__date_and_time__date').textContent = DATE;
 document.getElementById('hedaer__date_and_time__time').textContent = TIME;
+
+// Creating tasks and saving to localStorage
+const taskInput = document.querySelector('.block__task__input__entry_field')
+const taskList = document.querySelector('.task__text__list');
+const addTaskButton = document.querySelector('.block__task__button__text');
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+function schowTaskList() {
+    taskList.innerHTML = '';
+    tasks.forEach(function(task, index) {
+        const taskListItem = document.createElement('li');
+        taskListItem.innerHTML = `<input type='checkbox' ${task.done? 'checked' : ''} />
+        <span>${task.text}</span>`;
+        taskListItem.querySelector('input').addEventListener('change', function() {
+            tasks[index].done = this.checked;
+            updateLocalStorage();
+        });
+        taskList.append(taskListItem);
+    });
+};
+function updateLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+// Функция добавления задачи
+function createTask() {
+    let listText = taskInput.value.trim();
+    if (listText) {
+        tasks.push({text: listText, done: false});
+        taskInput.value = "";
+        updateLocalStorage();
+        schowTaskList();
+    }
+}
+addTaskButton.addEventListener('click', createTask);
+schowTaskList();
